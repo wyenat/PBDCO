0ea4df85680818b985389de1098a62087dee39ab
Vivien MATTER <Vivien.Matter@grenoble-inp.org>
2019-04-08 15:11:40 +0200

    Fabrique

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import InterfaceGraphique.ApplicationCapteurs;
 import InterfaceGraphique.ApplicationClient;
 import Ruche.Fabrique;
 
@@ -14,10 +15,6 @@ import Ruche.Fabrique;
  */
 public class Main {
     public static void main(String args[]){
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ApplicationClient().setVisible(true);
-            }
-        });
+        ApplicationCapteurs ac = new ApplicationCapteurs();
     }
 }
